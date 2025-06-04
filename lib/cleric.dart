65de503a051452e591b2a62178d0ea3bba4e7efd
Sujin Park <sujinlab@gmail.com>
2025-06-04 16:09:54 +0900

    feat: cleric에 pray메소드추가.

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   String name;
   int hp = 50;
@@ -8,6 +10,7 @@ class Cleric {
 
   Cleric(this.name, this.hp, this.mp);
 
+
   void selfAid() {
     if( mp >= 5 ) {
       mp -= 5;
@@ -16,5 +19,28 @@ class Cleric {
     } else {
       print('$name does not have enough MP to cast Self Aid.');
     } 
+  }
+
+  int pray(int praySeconds) {
+
+    //기도시간이 0이면, 회복못함
+    if( praySeconds <= 0 ) {
+      return 0;
+    }
+
+    Random random = Random();
+    int randomNumber = random.nextInt(3); //0~2
+    
+    //회복된 MP
+    int recoveredMP = praySeconds + randomNumber;
+
+    mp += recoveredMP;
+
+    //최대mp보다 회복할 순 없음.
+    if( mp > maxMp ) {
+      mp = maxMp;
+    }
+
+    return recoveredMP;
   }
 }
