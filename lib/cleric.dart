1a850b7950c8f8937a9f3c4a44a05e0b5f3aa0f8
Sujin Park <sujinlab@gmail.com>
2025-06-04 15:56:57 +0900

    feat: cleric에 sedfAid메소드추가.

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -7,4 +7,14 @@ class Cleric {
   static const int maxMp = 10;
 
   Cleric(this.name, this.hp, this.mp);
+
+  void selfAid() {
+    if( mp >= 5 ) {
+      mp -= 5;
+      hp = maxHp;
+      print('$name casts Self Aid! HP restored to $hp.');
+    } else {
+      print('$name does not have enough MP to cast Self Aid.');
+    } 
+  }
 }
