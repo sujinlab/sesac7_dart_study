f629270aa585f8eaa3c7524098262e062c6a2a53
Sujin Park <sujinlab@gmail.com>
2025-06-12 01:50:53 +0900

    remove: 배운것 테스트할때, 만든 파일들 삭제

--- lib/250611/collection_homework.dart
+++ lib/250611/collection_homework.dart
@@ -1,4 +1,4 @@
-import 'package:modu_3_dart_study/250611/study.dart';
+import 'package:modu_3_dart_study/250611/person.dart';
 
 void main() {
   //연습문제2
@@ -23,8 +23,8 @@ void main() {
 
   //연습문제3
   final people = <Person>[];
-  people.add(Person('홍길동'));
-  people.add(Person('한석봉'));
+  people.add(Person('홍길동', DateTime.utc(1979, 12, 12)));
+  people.add(Person('한석봉', DateTime.utc(1979, 12, 12)));
 
   for (final element in people) {
     print(element.name);
--- lib/250611/study2.dart
+++
@@ -1,8 +0,0 @@
-import 'package:modu_3_dart_study/250611/study.dart';
-
-void main() {
-  A a = A(1);
-
-  // print(a._energy);
-  print(a.energy);
-}
