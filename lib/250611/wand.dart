af7370e4e9bd1b2c335c4fbbc3e751711374e60e
Sujin Park <sujinlab@gmail.com>
2025-06-12 11:21:04 +0900

    feat: wand클래스 수정

--- lib/250611/wand.dart
+++ lib/250611/wand.dart
@@ -3,12 +3,21 @@ class Wand {
   double power;
 
   Wand({required this.name, required this.power}) {
-    if (name.length < 3) {
+    setName(name);
+    setPower(power);
+  }
+
+  void setName(String value) {
+    if (value.length < 3) {
       throw Exception('지팡이의 이름은 3문자이상');
     }
+    name = value;
+  }
 
-    if (power < 0.5 || power > 100) {
+  void setPower(double value) {
+    if (value < 0.5 || value > 100) {
       throw Exception('지팡이의 마력은 0.5이상 100.0이하여야 한다');
     }
+    power = value;
   }
 }
