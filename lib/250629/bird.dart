07a438d68519996876a2869004d0b829242fed5b
Sujin Park <sujinlab@gmail.com>
2025-06-29 23:18:49 +0900

    feat: movie클래스 추가, bird문제풀이

---
+++ lib/250629/bird.dart
@@ -0,0 +1,43 @@
+Future<void> bird1(Stopwatch stopwatch) async {
+  for (int i = 0; i < 4; i++) {
+    print('꾸우: ${stopwatch.elapsed}');
+    if (i != 3) {
+      await Future.delayed(Duration(seconds: 1));
+    }
+  }
+}
+
+Future<void> bird2(Stopwatch stopwatch) async {
+  for (int i = 0; i < 4; i++) {
+    print('까악: ${stopwatch.elapsed}');
+    if (i != 3) {
+      await Future.delayed(Duration(seconds: 2));
+    }
+  }
+}
+
+Future<void> bird3(Stopwatch stopwatch) async {
+  for (int i = 0; i < 4; i++) {
+    print('짹짹: ${stopwatch.elapsed}');
+    if (i != 3) {
+      await Future.delayed(Duration(seconds: 3));
+    }
+  }
+}
+
+void main() async {
+  // final futures = <Future<void>>[];
+  // futures.add(bird1());
+  // futures.add(bird2());
+  // futures.add(bird3());
+  final stopwatch = Stopwatch()..start();
+
+  List<Future<void>> futures = [
+    bird1(stopwatch),
+    bird2(stopwatch),
+    bird3(stopwatch),
+  ];
+
+  await Future.wait(futures);
+  print(stopwatch.elapsed);
+}
---
+++ lib/250629/movie.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+class Movie {
+  final String title;
+  final String director;
+  final int year;
+
+  Movie.fromJson(Map<String, dynamic> map)
+    : title = map['title'],
+      director = map['director'],
+      year = map['year'];
+
+  Map<String, dynamic> toJson() {
+    return {'title': title, 'director': director, 'year': year};
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! Movie) {
+      return false;
+    }
+
+    if (title == other.title &&
+        director == other.director &&
+        year == other.year) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(title.hashCode, director.hashCode, year.hashCode);
+
+  @override
+  String toString() {
+    return 'title: $title, director: $director, year: $year';
+  }
+}
+
+Future<Movie> getMovieInfo() async {
+  await Future.delayed(Duration(seconds: 2));
+
+  final String jsonString = '''
+  {
+    "title": "Star Ward",
+    "director": "George Lucas",
+    "year": 1977
+  }
+  ''';
+
+  return Movie.fromJson(jsonDecode(jsonString));
+}
+
+void main() async {
+  print(await getMovieInfo());
+}
