171d6ad63e43ae8fe42e99bfab84bdce06210ad6
Sujin Park <sujinlab@gmail.com>
2025-07-04 07:51:03 +0900

    test: Mock DataSource 제거 및 Mockito로 전환
    - Mock DataSource 클래스들을 삭제했습니다.
    - Mockito 라이브러리를 사용하여 DataSource에 대한 Mock 객체를 생성하고 테스트에 활용하도록 수정했습니다.

--- test/250703/comment_repository_impl_test.dart
+++ test/250703/comment_repository_impl_test.dart
@@ -1,27 +1,63 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:modu_3_dart_study/250703/data_source/comment_data_source.dart';
 import 'package:modu_3_dart_study/250703/repository/comment_repository_impl.dart';
 import 'package:test/test.dart';
-import 'mock/mock_comment_data_source_Impl.dart';
+import 'comment_repository_impl_test.mocks.dart';
 
+@GenerateMocks([CommentDataSource])
 void main() {
   group('CommentRepositoryImpl 테스트', () {
+    final json = [
+      {
+        "postId": 1,
+        "id": 1,
+        "name": "id labore ex et quam laborum",
+        "email": "Eliseo@gardner.biz",
+        "body":
+            "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium",
+      },
+      {
+        "postId": 1,
+        "id": 2,
+        "name": "quo vero reiciendis velit similique earum",
+        "email": "Jayne_Kuhic@sydney.com",
+        "body":
+            "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+      },
+      {
+        "postId": 2,
+        "id": 3,
+        "name": "odio adipisci rerum aut animi",
+        "email": "Nikita@garfield.biz",
+        "body":
+            "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione",
+      },
+    ];
+
     test('getComments(1)', () async {
-      final CommentDataSource dataSource = MockCommentDataSourceImpl();
+      final CommentDataSource dataSource = MockCommentDataSource();
       final commentRepo = CommentRepositoryImpl(dataSource);
+
+      when(dataSource.getComments()).thenAnswer((_) async => json);
 
       expect((await commentRepo.getComments(1)).length, 2);
     });
 
     test('getComments(2)', () async {
-      final CommentDataSource dataSource = MockCommentDataSourceImpl();
+      final CommentDataSource dataSource = MockCommentDataSource();
       final commentRepo = CommentRepositoryImpl(dataSource);
+
+      when(dataSource.getComments()).thenAnswer((_) async => json);
 
       expect((await commentRepo.getComments(2)).length, 1);
     });
 
     test('getComments(3)', () async {
-      final CommentDataSource dataSource = MockCommentDataSourceImpl();
+      final CommentDataSource dataSource = MockCommentDataSource();
       final commentRepo = CommentRepositoryImpl(dataSource);
+
+      when(dataSource.getComments()).thenAnswer((_) async => json);
 
       expect((await commentRepo.getComments(3)).length, 0);
     });
--- test/250703/todo_repository_impl_test.dart
+++ test/250703/todo_repository_impl_test.dart
@@ -1,22 +1,60 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:modu_3_dart_study/250703/data_source/todo_data_source.dart';
 import 'package:modu_3_dart_study/250703/repository/todo_repository_impl.dart';
 import 'package:test/test.dart';
+import 'todo_repository_impl_test.mocks.dart';
 
-import 'mock/mock_todo_data_source_impl.dart';
-
+@GenerateMocks([TodoDataSource])
 void main() {
   group('TodoRepositoryImpl 테스트', () {
+    final json = [
+      {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false,
+      },
+      {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false,
+      },
+      {
+        "userId": 1,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false,
+      },
+      {
+        "userId": 1,
+        "id": 4,
+        "title": "et porro tempora",
+        "completed": true,
+      },
+      {
+        "userId": 1,
+        "id": 5,
+        "title":
+            "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false,
+      },
+    ];
+
     test('getTodos()', () async {
-      final TodoDataSource dataSource = MockTodoDataSourceImpl();
+      final TodoDataSource dataSource = MockTodoDataSource();
       final todoRepo = TodoRepositoryImpl(dataSource);
 
+      when(dataSource.getTodos()).thenAnswer((_) async => json);
       expect((await todoRepo.getTodos()).length, 5);
     });
 
     test('getCompletedTodos()', () async {
-      final TodoDataSource dataSource = MockTodoDataSourceImpl();
+      final TodoDataSource dataSource = MockTodoDataSource();
       final todoRepo = TodoRepositoryImpl(dataSource);
 
+      when(dataSource.getTodos()).thenAnswer((_) async => json);
       expect((await todoRepo.getCompletedTodos()).length, 1);
     });
   });
